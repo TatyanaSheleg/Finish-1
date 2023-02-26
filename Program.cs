49544c5c97c67ae9a8ec2171913f242d07bc84f9
Tatiana Sheleh <apsua@tut.by>
2023-02-26 13:48:14 +0300

    программа

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите слова через запятую: ");
+string text = Console.ReadLine();
+char[] separators = new char[] { ',', ' ' };
+string[] words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+int count = 0;
+foreach (string word in words)
+{
+    if (word.Length < 4)
+    {
+        Console.WriteLine(word);
+        count++;
+    }
+}
+if (count == 0)
+    Console.WriteLine("В заданной строке отсутствуют слова с количеством символов равным или меньше 3");
